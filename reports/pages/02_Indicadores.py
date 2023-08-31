0473e6bd9d2e48d47d7e3adaeff15abef6e58083
Eduardo Gomes <edu_merckx@yahoo.com.br>
2023-08-30 22:00:34 -0300

    update 'Indicadores' page

--- reports/pages/02_Indicadores.py
+++ reports/pages/02_Indicadores.py
@@ -1,8 +1,47 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Tech Challenge #02 - Grupo 27",
-    layout="wide"
-)
-
 st.title('Indicadores')
+
+tab_bb, tab_rsi, tab_macd = st.tabs(['Bolling Bands', 'RSI', 'MACD'])
+
+with tab_bb:
+    st.markdown("""
+        A utilização das Bandas de Bollinger em nosso modelo visa identificar momentos de alta volatilidade e possíveis pontos de reversão. 
+        Isso é essencial para entender os cenários em que o mercado pode estar passando por mudanças substanciais, permitindo-nos ajustar nossas previsões para refletir a volatilidade atual.
+    """)
+
+    st.markdown("""
+        Tomando como exemplo o gráfico de 2022, a partir da análise com bandas de bollinger podem nos indicar sinais de reversão da tendência atual. 
+        Quando o volume de fechamento ultrapassa uma das bandas, isso pode indicar uma possível reversão da tendência atual. 
+        Por exemplo, uma quebra acima da Banda de Bollinger superior pode sugerir uma possível reversão para baixa volatilidade ou uma mudança na tendência de baixa para alta.
+    """)
+
+with tab_rsi:
+    st.markdown("""
+        Optamos por utilizá-lo para auxiliar o nosso modelo a nos fornecer insights sobre a força de uma tendência, 
+        indicando em quais momentos a IBOVESPA estava em momentos de "sobrecomprado" (potencialmente devido a uma alta excessiva) 
+        ou "sobrevendido" (possivelmente causado por uma queda excessiva nos preços).
+
+        Ao incorporar o RSI em nosso modelo, buscamos capturar possíveis reversões de tendência com base nas condições de sobrecompra e sobrevenda, 
+        permitindo que nosso modelo identifique esses pontos e ajuste suas previsões.
+    """)
+
+    st.markdown("""
+        O RSI é varia entre 0 e 100. Tradicionalmente, valores acima de 70 indicam uma condição de sobrecompra, 
+        sugerindo que o ativo pode estar prestes a sofrer uma reversão de baixa. 
+        Valores abaixo de 30 indicam uma condição de sobrevenda, sugerindo que o ativo pode estar prestes a se recuperar.
+    """)
+
+with tab_macd:
+    st.markdown("""
+        Utilizamos o MACD para obter informações sobre a relação das médias móveis, indicando a força e a direção de uma tendência atual.  
+        Além disso, conseguimos obter o dado de velocidade com que ela está se desenvolvendo. 
+        Isso nos permite ajustar nossas previsões com base em mudanças abruptas ou gradualidades na evolução dos preços, 
+        contribuindo para uma compreensão mais abrangente da dinâmica do mercado.
+    """)
+
+    st.markdown("""
+        #Quando há cruzamentos entre o MACD e a linha de sinal (o MACD cruza acima ou abaixo da linha de sinal) e esse cruzamento 
+        coincide com movimentos significativos no preço de fechamento, isso pode ser considerado um sinal mais forte. 
+        Por exemplo, se o MACD cruzar acima da linha de sinal ao mesmo tempo em que o preço de fechamento está subindo, isso pode indicar uma possível tendência de alta mais forte.
+    """)
--- reports/pages/03_Conclusão.py
+++ reports/pages/03_Conclusão.py
@@ -1,8 +1,3 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Tech Challenge #02 - Grupo 27",
-    layout="wide"
-)
-
 st.title('Conclusão')
